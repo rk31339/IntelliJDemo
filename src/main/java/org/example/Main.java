243832eb336796cc1ecfb34369221fa40b6277ee
Rafiq Khan <rafiqkhan.pathan@gmail.com>
2022-12-24 21:53:54 +0800

    removed 2nd sout

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world !");
-        System.out.println("this is test project-for GIT test !");
+        System.out.println("Hello World Rafiq!");
     }
 }
